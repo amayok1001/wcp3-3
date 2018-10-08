16b38627cd12dbf1acfe2d8ea9f62b6b18715b03
Erika <Erika@Erika-no-MacBook-Air.local>
2018-10-08 17:47:57 +0900

    [Finish]3-6

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,13 +8,14 @@ class PostsController < ApplicationController
       post = Post.new(post_params)
     # DBへ保存する
       post.save
-    # トップ画面へリダイレクト
-      redirect_to '/top'
+    # 詳細画面へリダイレクト
+      redirect_to post_path(post.id)
   end
   def index
       @posts = Post.all
   end
   def show
+      @post = Post.find(params[:id])
   end
 
     #privateと書くとアクションとして認識されない
